ec469c4def7fe50a264858abf07f97b48f4a06d9
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-04-12 19:34:41 +0500

    added data deletion

---
+++ pandas9.py
@@ -0,0 +1,12 @@
+import pandas as pd
+data ={
+  "Name":["Sameer","Rizwan","Usoof","Jamal"],
+  "Age":[24,23,26,28],"Salary":[100000,120000,95000,150000]
+  ,"Experience":[1,1.5,1,2]
+}
+df = pd.DataFrame(data)
+print(df)
+# Removing column
+df.drop(columns=["Age"],inplace=True)
+print(df)
+#df.drop(columns=["Salary","Experience"],inplace=True)
